1bfacec4897c8c6cf1e94586badecf191fe33078
Alexander Kamenev <75051539+Nraj10@users.noreply.github.com>
2022-06-25 22:11:00 +0300

    Фикс
    Теперь объекты улетают под карту перед удалением

--- Assets/Scripts/PickableItem.cs
+++ Assets/Scripts/PickableItem.cs
@@ -8,19 +8,30 @@ public class PickableItem : Interactable
     [SerializeField] public PlayerController ctr;
     [SerializeField] public int amount;
 
+
     public override void OnFocus()
     {
     }
 
     public override void OnInteract()
     {
+        GameObject.Find("SubtitlesInfo").GetComponent<TMPro.TextMeshProUGUI>().text = "Picked Up <color=green>" + item.name + "</color>";
         ctr = GameObject.Find("Player").GetComponent<PlayerController>();
         ctr.inventory.AddItem(item, amount);
+        StartCoroutine(ExecuteAfterTime(3));
+        gameObject.transform.position = new Vector3(0,-100,0);
+    }
+
+    public IEnumerator ExecuteAfterTime(float timeInSec)
+    {
+        yield return new WaitForSeconds(timeInSec);
+        GameObject.Find("SubtitlesInfo").GetComponent<TMPro.TextMeshProUGUI>().text = "";
         Destroy(gameObject);
     }
 
     public override void OnLoseFocus()
     {
     }
+    
 
 }
